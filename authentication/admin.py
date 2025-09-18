a4993f7667069dc48e6cd61824ce82c5fa9bda51
Le-dev-du-coin <ledevducoin223@gmail.com>
2025-09-18 19:53:16 +0000

    feat(admin): enable creating Admin/Agent (Chine/Mali) via Django Admin; add favicon to base template

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser, PasswordResetToken
 
-# Register your models here.
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ("telephone", "email", "first_name", "last_name", "role")
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = ("telephone", "email", "first_name", "last_name", "role", "is_active", "is_staff", "is_superuser", "groups", "user_permissions")
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    ordering = ("telephone",)
+    list_display = ("telephone", "email", "first_name", "last_name", "role", "is_active", "is_staff")
+    list_filter = ("role", "is_active", "is_staff", "is_superuser")
+    search_fields = ("telephone", "email", "first_name", "last_name")
+
+    fieldsets = (
+        (None, {"fields": ("telephone", "password")}),
+        ("Informations personnelles", {"fields": ("first_name", "last_name", "email")}),
+        ("Rôles et permissions", {"fields": ("role", "is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Dates importantes", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("telephone", "email", "first_name", "last_name", "role", "password1", "password2", "is_active", "is_staff")
+        }),
+    )
+
+    # Le modèle n'a pas d'username, on ajuste les champs
+    filter_horizontal = ("groups", "user_permissions")
+    readonly_fields = ("last_login", "date_joined")
+
+
+@admin.register(PasswordResetToken)
+class PasswordResetTokenAdmin(admin.ModelAdmin):
+    list_display = ("user", "token", "created_at", "expires_at", "used")
+    list_filter = ("used", "created_at")
+    search_fields = ("user__telephone", "user__email", "token")
